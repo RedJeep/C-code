889e8a44a0529441fc78050ef1d1562328820759
RedJeep <jgblue@live.com>
2018-03-15 16:08:34 -0400

    Add files via upload

---
+++ r.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int radius,area;
+	printf("Enter radius( i.e 10)");
+	scanf("%d",&radius);
+	area=3.14159*radius *radius;
+	printf( "\n\n area= %d", area);
+	return 0;
+	
+	
+	
+}
